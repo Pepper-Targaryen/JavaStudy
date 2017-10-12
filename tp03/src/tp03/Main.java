32b191d45bef1974ef616a6e2deee5dc8e730355
Yixiao Fei <targaryenpepper@gmail.com>
2017-10-12 21:26:14 +0200

    End of Heap Sort

--- tp03/src/tp03/Main.java
+++ tp03/src/tp03/Main.java
@@ -1,10 +1,13 @@
 package tp03;
+import structures.Heap;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World");
+		Heap wordsHeap = new Heap(args);
+		String[] temp= wordsHeap.sort();
+		for(String a:temp) System.out.println(a);
 	}
 
 }
